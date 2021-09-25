4a9093b3a8782eb55d0a7d7e464afdfc09b1f3fe
selimyalinkilic <yalinkilicselim@gmail.com>
2021-09-25 16:29:49 +0300

    Update index.js
    added new product fetch

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,35 @@
 import Link from 'next/link'
 import Layout from '../components/layout'
-import Head from 'next/head'
+import Head from 'next/head';
 
-function HomePage({ products }) {    
+export const getStaticProps = async () => {
+  // data fetch  
+  const res = await fetch('https://fakestoreapi.com/products');
+  const data = await res.json();
+
+  return {
+    props: {
+      products: data
+    }, // will be passed to the page component as props
+  }  
+}
+
+function HomePage({ products }) {  
+  console.log(products);
   return (
     <Layout>
       <Head>
         <title>Anasayfa</title>
       </Head>      
+      <div>
+        {
+          products.map(product => (
+            <div key={product.id}>{product.title}</div>
+          ))
+        }        
+      </div>
     </Layout>
   )
-}
-
-export async function getStaticProps(context) {
-  // data fetch
-  const res = await fetch(`https://fakestoreapi.com/products`)
-  const data = await res.json()
-  return {
-    props: {
-      products:[data]
-    }, // will be passed to the page component as props
-  }
 }
 
 export default HomePage
